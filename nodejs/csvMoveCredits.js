8066652acf990c7ce8e2e9237c9fbb6832505c8e
David Crowther <crowtherdr@gmail.com>
2023-11-22 21:04:14 -0700

    Splits files every nth lines

--- nodejs/csvMoveCredits.js
+++ nodejs/csvMoveCredits.js
@@ -1,31 +1,55 @@
 const fs = require('fs')
 const { parse } = require("csv-parse")
-const { stringify } = require("csv-stringify")
 
 const arguments = process.argv
 if (arguments?.length < 3) {
   throw new Error('Missing filename')
 }
-const filename = `./${arguments[2]}`
-const writableStream = fs.createWriteStream(`${filename}-converted.csv`)
+const baseFilename = arguments[2]
+const infileName = `./${baseFilename}.csv`
+let fileCount = 1
+let count = 0
+const MAXIMUM_LINES_IN_FILE = 800
+let outStream
+let outfileName = `${baseFilename}-${fileCount}.csv`
+newWriteStream()
+let inStream = fs.createReadStream(infileName)
 
-const stringifier = stringify({ header: false })
-fs.createReadStream(`${filename}.csv`)
-  .pipe(parse({ delimiter: ",", from_line: 3 }))
-  .on("data", function (row) {
-    const dateTimeArray = row[2].split('T')
-    row[2] = dateTimeArray[0]
-    if (row[8]?.startsWith('-')) {
-      row[9] = row[8]
-      row[8] = ''
-    }
-    console.log(row)
-    stringifier.write(row)
-  })
-  .on("end", function () {
-    stringifier.pipe(writableStream)
-    console.log("finished")
-  })
-  .on("error", function (error) {
-    console.log(error.message)
-  })
+function newWriteStream(){
+  outfileName = `${baseFilename}-${fileCount}.csv`
+  outStream = fs.createWriteStream(outfileName)
+  count = 0
+}
+
+const updateLine = (lineArray) => {
+  const dateTimeArray = lineArray[2]?.split('T') || []
+  lineArray[2] = dateTimeArray[0] || []
+  if (lineArray[8]?.startsWith('-')) {
+    lineArray[9] = lineArray[8]
+    lineArray[8] = ''
+  }
+  return JSON.stringify(lineArray)
+}
+
+inStream.pipe(parse({ delimiter: ",", from_line: 3 }))
+.on("data", function (line) {
+  count++
+  const updatedLine = updateLine(line)
+  outStream.write(updatedLine + '\n')
+
+  if (count >= MAXIMUM_LINES_IN_FILE) {
+    fileCount++
+    console.log('file ', outfileName, count)
+    outStream.end()
+    newWriteStream()
+  }
+})
+
+inStream.on('close', function() {
+  if (count > 0) {
+      console.log('Final close:', outfileName, count)
+  }
+  inStream.close()
+  outStream.end()
+  console.log('Done')
+})
